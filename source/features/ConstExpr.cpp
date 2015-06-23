488e0295f92210d526a1c0774491e74d87c88a4e
Eric Parayre <eric.parayre@gmail.com>
2015-06-22 22:47:35 -0400

    fixed constexpr tests, c++11 implementation was inverted with the c++14 one

--- source/features/ConstExpr.cpp
+++ source/features/ConstExpr.cpp
@@ -4,19 +4,19 @@
 
 namespace
 {
-#ifdef CPP14_CONSTEXPR
-    //-----------------------------------------------------------------------------
-    constexpr int Pow( int base, int exp ) noexcept
-    {
-        return ( exp == 0 ? 1 : base * Pow( base, exp - 1 ) );
-    }
-#else
+#if defined( CPP14_CONSTEXPR )
     //-----------------------------------------------------------------------------
     constexpr int Pow( int base, int exp ) noexcept
     {
         auto result = 1;
         for( int i = 0; i < exp; ++i ) result *= base;
         return result;
+    }
+#else
+    //-----------------------------------------------------------------------------
+    constexpr int Pow( int base, int exp ) noexcept
+    {
+        return ( exp == 0 ? 1 : base * Pow( base, exp - 1 ) );
     }
 #endif
 }
