9961757f1ed244dc34a8a150aa53abb16211b7bd
Eric Parayre <eric.parayre@gmail.com>
2015-06-24 02:37:56 -0400

    attempt to fix gcc c++11 feature detection

--- source/config/compiler_gcc.hpp
+++ source/config/compiler_gcc.hpp
@@ -8,42 +8,42 @@
     //-----------------------------------------------------------------------------
     // setup
 
-    #define GCC_VERSION ( ( __GNUC__ * 10000 ) + ( __GNUC_MINOR__ * 100 ) + ( __GNUC_PATCHLEVEL__ ) )
+    #define GCC_VERSION ( __GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__ )
+
+    #if defined( __GXX_EXPERIMENTAL_CXX0X__ )
+        #define GCC_CXX11
+    #endif
 
     //-----------------------------------------------------------------------------
     // c++11
 
-    #if defined( __GXX_EXPERIMENTAL_CXX0X__ )
+    #if ( GCCVERSION >= 40300 ) && defined( GCC_CXX11 ) // gcc 4.3
+        #define CXX11_DECLTYPE
+    #endif
 
-        #if ( GCCVERSION >= 40300 ) // gcc 4.3
-            #define CXX11_DECLTYPE
-        #endif
+    #if ( GCCVERSION >= 40400 ) && defined( GCC_CXX11 ) // gcc 4.4
+        #define CXX11_DEFAULTED_FUNCTIONS
+        #define CXX11_DELETED_FUNCTIONS
+        #define CXX11_VARIADIC_TEMPLATES
+    #endif
 
-        #if ( GCCVERSION >= 40400 ) // gcc 4.4
-            #define CXX11_DEFAULTED_FUNCTIONS
-            #define CXX11_DELETED_FUNCTIONS
-            #define CXX11_VARIADIC_TEMPLATES
-        #endif
+    #if ( GCCVERSION >= 40500 ) && defined( GCC_CXX11 ) // gcc 4.5
+        #define CXX11_LAMBDAS
+    #endif
 
-        #if ( GCCVERSION >= 40500 ) // gcc 4.5
-            #define CXX11_LAMBDAS
-        #endif
+    #if ( GCCVERSION >= 40501 ) && defined( GCC_CXX11 ) // gcc 4.5.1
+        #define CXX11_STRONG_ENUMS
+    #endif
 
-        #if ( GCCVERSION >= 40501 ) // gcc 4.5.1
-            #define CXX11_STRONG_ENUMS
-        #endif
+    #if ( GCCVERSION >= 40600 ) && defined( GCC_CXX11 ) // gcc 4.6
+        #define CXX11_CONSTEXPR
+        #define CXX11_GENERALIZED_INITIALIZERS
+        #define CXX11_NOEXCEPT
+        #define CXX11_NULLPTR
+    #endif
 
-        #if ( GCCVERSION >= 40600 ) // gcc 4.6
-            #define CXX11_CONSTEXPR
-            #define CXX11_GENERALIZED_INITIALIZERS
-            #define CXX11_NOEXCEPT
-            #define CXX11_NULLPTR
-        #endif
-
-        #if ( GCCVERSION >= 40700 ) // gcc 4.7
-            #define CXX11_ALIAS_TEMPLATES
-        #endif
-
+    #if ( GCCVERSION >= 40700 ) && defined( GCC_CXX11 ) // gcc 4.7
+        #define CXX11_ALIAS_TEMPLATES
     #endif
 
     //-----------------------------------------------------------------------------
@@ -52,10 +52,5 @@
     #if ( GCCVERSION >= 40900 ) // gcc 4.9
         #define CXX14_DECLTYPE_AUTO
     #endif
-
-    //-----------------------------------------------------------------------------
-    // cleanup
-
-    #undef GCC_VERSION
 
 #endif
