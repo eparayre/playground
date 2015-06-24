7649aef09680eaa0e27ca0e3f278a21512045feb
Eric Parayre <eric.parayre@gmail.com>
2015-06-24 09:24:01 -0400

    make sure gcc does not overshadow clang

--- source/config/compiler_gcc.hpp
+++ source/config/compiler_gcc.hpp
@@ -3,12 +3,12 @@
 //-----------------------------------------------------------------------------
 // gcc
 
-#if defined( __GNUC__ )
+#if defined( __GNUC__ ) && !defined( __clang__ )
 
     #define GCC_VERSION ( __GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__ )
 
-    #define COMPILER_STR(a) #a
-    #define COMPILER_XSTR(a) COMPILER_STR(a)
+    #define COMPILER_STR( a ) #a
+    #define COMPILER_XSTR( a ) COMPILER_STR( a )
 
     #define COMPILER_IDENTIFICATION "gcc-" COMPILER_XSTR( __GNUC__ ) "." COMPILER_XSTR( __GNUC_MINOR__ ) "." COMPILER_XSTR( __GNUC_PATCHLEVEL__ )
 
