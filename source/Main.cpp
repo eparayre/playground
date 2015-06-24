586097c93d77319f91795ef9e0d5c5d24e9bb6c0
Eric Parayre <eric.parayre@gmail.com>
2015-06-24 05:56:41 -0400

    modified compiler features dump to be more compact

--- source/Main.cpp
+++ source/Main.cpp
@@ -8,74 +8,72 @@ namespace
     //-----------------------------------------------------------------------------
     void DumpCxx11Features()
     {
-        std::cout << std::endl << "C++11 :" << std::endl;
-
     #ifdef CXX11_ALIAS_TEMPLATES
-        std::cout << "> cxx_alias_templates" << std::endl;
+        std::cout << "> cxx11_alias_templates" << std::endl;
     #endif
 
     #ifdef CXX11_CONSTEXPR
-        std::cout << "> cxx_constexpr" << std::endl;
+        std::cout << "> cxx11_constexpr" << std::endl;
     #endif
 
     #ifdef CXX11_DECLTYPE
-        std::cout << "> cxx_decltype" << std::endl;
+        std::cout << "> cxx11_decltype" << std::endl;
     #endif
 
     #ifdef CXX11_DEFAULTED_FUNCTIONS
-        std::cout << "> cxx_defaulted_functions" << std::endl;
+        std::cout << "> cxx11_defaulted_functions" << std::endl;
     #endif
 
     #ifdef CXX11_DELETED_FUNCTIONS
-        std::cout << "> cxx_deleted_functions" << std::endl;
+        std::cout << "> cxx11_deleted_functions" << std::endl;
     #endif
 
     #ifdef CXX11_GENERALIZED_INITIALIZERS
-        std::cout << "> cxx_generalized_initializers" << std::endl;
+        std::cout << "> cxx11_generalized_initializers" << std::endl;
     #endif
 
     #ifdef CXX11_IMPLICIT_MOVES
-        std::cout << "> cxx_implicit_moves" << std::endl;
+        std::cout << "> cxx11_implicit_moves" << std::endl;
     #endif
 
     #ifdef CXX11_LAMBDAS
-        std::cout << "> cxx_lambdas" << std::endl;
+        std::cout << "> cxx11_lambdas" << std::endl;
     #endif
 
     #ifdef CXX11_NOEXCEPT
-        std::cout << "> cxx_noexcept" << std::endl;
+        std::cout << "> cxx11_noexcept" << std::endl;
     #endif
 
     #ifdef CXX11_NULLPTR
-        std::cout << "> cxx_nullptr" << std::endl;
+        std::cout << "> cxx11_nullptr" << std::endl;
     #endif
 
     #ifdef CXX11_STRONG_ENUMS
-        std::cout << "> cxx_strong_enums" << std::endl;
+        std::cout << "> cxx11_strong_enums" << std::endl;
     #endif
 
     #ifdef CXX11_VARIADIC_TEMPLATES
-        std::cout << "> cxx_variadic_templates" << std::endl;
+        std::cout << "> cxx11_variadic_templates" << std::endl;
     #endif
     }
 
     //-----------------------------------------------------------------------------
     void DumpCxx14Features()
     {
-        std::cout << std::endl << "C++14 :" << std::endl;
-
     #ifdef CXX14_DECLTYPE_AUTO
-        std::cout << "> cxx_decltype_auto" << std::endl;
+        std::cout << "> cxx14_decltype_auto" << std::endl;
     #endif
 
     #ifdef CXX14_RELAXED_CONSTEXPR
-        std::cout << "> cxx_relaxed_constexpr" << std::endl;
+        std::cout << "> cxx14_relaxed_constexpr" << std::endl;
     #endif
     }
 
     //-----------------------------------------------------------------------------
     void DumpCxxFeatures()
     {
+        std::cout << std::endl;
+
         DumpCxx11Features();
         DumpCxx14Features();
 
