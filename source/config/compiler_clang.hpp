2c5d09a9a3064bfd7591322b8d2a324c4dae4fcb
Eric Parayre <eric.parayre@gmail.com>
2015-06-24 02:48:21 -0400

    do not redefine COMPILER_NAME for clang

--- source/config/compiler_clang.hpp
+++ source/config/compiler_clang.hpp
@@ -10,7 +10,6 @@
 
     #define CLANG_VERSION ( __clang_major__ * 10000 + __clang_minor__ * 100 )
 
-    #define COMPILER_NAME "clang"
     #define COMPILER_VERSION CLANG_VERSION
 
     //-----------------------------------------------------------------------------
