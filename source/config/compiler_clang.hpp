d47a6181d8b8fe8be99e85027303470786bdd0bb
Eric Parayre <eric.parayre@gmail.com>
2016-03-08 00:05:33 -0500

    meh, restored clang and gcc feature detection as before

--- source/config/compiler_clang.hpp
+++ source/config/compiler_clang.hpp
@@ -10,6 +10,78 @@
 
     #define COMPILER_IDENTIFICATION "clang-" COMPILER_XSTR( __clang_major__ ) "." COMPILER_XSTR( __clang_minor__ ) "." COMPILER_XSTR( __clang_patchlevel__ )
 
-    #include "config/compiler_has_feature.hpp"
+    //-----------------------------------------------------------------------------
+    // c++11
+
+    #if __has_feature( cxx_alias_templates )
+        #define CXX11_ALIAS_TEMPLATES
+    #endif
+
+    #if __has_feature( cxx_constexpr )
+        #define CXX11_CONSTEXPR
+    #endif
+
+    #if __has_feature( cxx_decltype )
+        #define CXX11_DECLTYPE
+    #endif
+
+    #if __has_feature( cxx_defaulted_functions )
+        #define CXX11_DEFAULTED_FUNCTIONS
+    #endif
+
+    #if __has_feature( cxx_delegating_constructors )
+        #define CXX11_DELEGATING_CONSTRUCTORS
+    #endif
+
+    #if __has_feature( cxx_deleted_functions )
+        #define CXX11_DELETED_FUNCTIONS
+    #endif
+
+    #if __has_feature( cxx_generalized_initializers )
+        #define CXX11_GENERALIZED_INITIALIZERS
+    #endif
+
+    #if __has_feature( cxx_implicit_moves )
+        #define CXX11_IMPLICIT_MOVES
+    #endif
+
+    #if __has_feature( cxx_inheriting_constructors )
+        #define CXX11_INHERITING_CONSTRUCTORS
+    #endif
+
+    #if __has_feature( cxx_lambdas )
+        #define CXX11_LAMBDAS
+    #endif
+
+    #if __has_feature( cxx_noexcept )
+        #define CXX11_NOEXCEPT
+    #endif
+
+    #if __has_feature( cxx_nullptr )
+        #define CXX11_NULLPTR
+    #endif
+
+    #if __has_feature( cxx_strong_enums )
+        #define CXX11_STRONG_ENUMS
+    #endif
+
+    #if __has_feature( cxx_variadic_templates )
+        #define CXX11_VARIADIC_TEMPLATES
+    #endif
+
+    //-----------------------------------------------------------------------------
+    // c++14
+
+    #if __has_feature( __cxx_decltype_auto__ )
+        #define CXX14_DECLTYPE_AUTO
+    #endif
+
+    #if __has_feature( __cxx_init_captures__ )
+        #define CXX14_INIT_CAPTURES
+    #endif
+
+    #if __has_feature( cxx_relaxed_constexpr )
+        #define CXX14_RELAXED_CONSTEXPR
+    #endif
 
 #endif
--- source/config/compiler_gcc.hpp
+++ source/config/compiler_gcc.hpp
@@ -12,63 +12,55 @@
 
     #define COMPILER_IDENTIFICATION "gcc-" COMPILER_XSTR( __GNUC__ ) "." COMPILER_XSTR( __GNUC_MINOR__ ) "." COMPILER_XSTR( __GNUC_PATCHLEVEL__ )
 
-    #if defined( __has_feature )
+    //-----------------------------------------------------------------------------
+    // c++11
 
-        #include "config/compiler_has_feature.hpp"
+    #if defined( __GXX_EXPERIMENTAL_CXX0X__ )
 
-    #else
-
-        //-----------------------------------------------------------------------------
-        // c++11
-
-        #if defined( __GXX_EXPERIMENTAL_CXX0X__ )
-
-            #if ( GCC_VERSION >= 40300 ) // gcc 4.3
-                #define CXX11_DECLTYPE
-            #endif
-
-            #if ( GCC_VERSION >= 40400 ) // gcc 4.4
-                #define CXX11_DEFAULTED_FUNCTIONS
-                #define CXX11_DELETED_FUNCTIONS
-                #define CXX11_VARIADIC_TEMPLATES
-            #endif
-
-            #if ( GCC_VERSION >= 40500 ) // gcc 4.5
-                #define CXX11_LAMBDAS
-            #endif
-
-            #if ( GCC_VERSION >= 40501 ) // gcc 4.5.1
-                #define CXX11_STRONG_ENUMS
-            #endif
-
-            #if ( GCC_VERSION >= 40600 ) // gcc 4.6
-                #define CXX11_CONSTEXPR
-                #define CXX11_GENERALIZED_INITIALIZERS
-                #define CXX11_NOEXCEPT
-                #define CXX11_NULLPTR
-            #endif
-
-            #if ( GCC_VERSION >= 40700 ) // gcc 4.7
-                #define CXX11_ALIAS_TEMPLATES
-            #endif
-
+        #if ( GCC_VERSION >= 40300 ) // gcc 4.3
+            #define CXX11_DECLTYPE
         #endif
 
-        //-----------------------------------------------------------------------------
-        // c++14
-
-        #if ( GCC_VERSION >= 40900 ) // gcc 4.9
-            #define CXX14_DECLTYPE_AUTO
+        #if ( GCC_VERSION >= 40400 ) // gcc 4.4
+            #define CXX11_DEFAULTED_FUNCTIONS
+            #define CXX11_DELETED_FUNCTIONS
+            #define CXX11_VARIADIC_TEMPLATES
         #endif
 
-        #if ( GCC_VERSION >= 40900 ) // gcc 4.9
-            #define CXX14_INIT_CAPTURES
+        #if ( GCC_VERSION >= 40500 ) // gcc 4.5
+            #define CXX11_LAMBDAS
         #endif
 
-        #if defined( __cpp_constexpr ) && ( __cpp_constexpr >= 201304 )
-            #define CXX14_RELAXED_CONSTEXPR
+        #if ( GCC_VERSION >= 40501 ) // gcc 4.5.1
+            #define CXX11_STRONG_ENUMS
         #endif
 
+        #if ( GCC_VERSION >= 40600 ) // gcc 4.6
+            #define CXX11_CONSTEXPR
+            #define CXX11_GENERALIZED_INITIALIZERS
+            #define CXX11_NOEXCEPT
+            #define CXX11_NULLPTR
+        #endif
+
+        #if ( GCC_VERSION >= 40700 ) // gcc 4.7
+            #define CXX11_ALIAS_TEMPLATES
+        #endif
+
+    #endif
+
+    //-----------------------------------------------------------------------------
+    // c++14
+
+    #if ( GCC_VERSION >= 40900 ) // gcc 4.9
+        #define CXX14_DECLTYPE_AUTO
+    #endif
+
+    #if ( GCC_VERSION >= 40900 ) // gcc 4.9
+        #define CXX14_INIT_CAPTURES
+    #endif
+
+    #if defined( __cpp_constexpr ) && ( __cpp_constexpr >= 201304 )
+        #define CXX14_RELAXED_CONSTEXPR
     #endif
 
 #endif
