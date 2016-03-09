809186397e72043204feec1c34f024c82f5afe21
Eric Parayre <eric.parayre@gmail.com>
2016-03-08 22:25:07 -0500

    DelegatingConstructors * tested calling the default ctor from another ctor

--- source/features/DelegatingConstructors.cpp
+++ source/features/DelegatingConstructors.cpp
@@ -2,25 +2,54 @@
 
 #if defined( CXX11_DELEGATING_CONSTRUCTORS )
 
+#include <string>
+
 namespace
 {
     //-----------------------------------------------------------------------------
-    class DelegatingConstructors
+    class SampleA
     {
     public:
-        DelegatingConstructors()
-            : DelegatingConstructors(true, 11, 22)
+        SampleA()
+        {
+            message_ = "must pass through the default ctor";
+        }
+
+        SampleA(float value)
+            : SampleA()
+
+            // error: an initializer for a delegating constructor must appear alone
+            /*, value_(3)*/
+        {
+            // but we can initialize members in the body
+            value_ = value;
+        }
+
+        float GetValue() const { return value_; }
+        const std::string& GetMessage() const { return message_;}
+
+    private:
+        float value_{1.1f};
+        std::string message_{"invalid"};
+    };
+
+    //-----------------------------------------------------------------------------
+    class SampleB
+    {
+    public:
+        SampleB()
+            : SampleB(true, 11, 22)
         {}
 
-        explicit DelegatingConstructors(bool boolean)
-            : DelegatingConstructors(boolean, 33, 44)
+        explicit SampleB(bool boolean)
+            : SampleB(boolean, 33, 44)
         {}
 
-        DelegatingConstructors(bool boolean, int intA)
-            : DelegatingConstructors(boolean, intA, 66)
+        SampleB(bool boolean, int intA)
+            : SampleB(boolean, intA, 66)
         {}
 
-        DelegatingConstructors(bool boolean, int intA, int intB)
+        SampleB(bool boolean, int intA, int intB)
             : boolean_(boolean)
             , intA_(intA)
             , intB_(intB)
@@ -40,40 +69,51 @@ namespace
 
 TEST_CASE("Delegating Constructors", "[c++11][delegating_constructors]")
 {
-    SECTION("Default Ctor")
+    SECTION("Calling default ctor")
     {
-        DelegatingConstructors object;
+        SampleA object(7.0f);
 
-        CHECK(object.GetBoolean() == true);
-        CHECK(object.GetIntA() == 11);
-        CHECK(object.GetIntB() == 22);
+        CHECK(object.GetMessage() == "must pass through the default ctor");
+        CHECK(object.GetValue() == 7.0f);
     }
 
-    SECTION("Ctor 1")
+    SECTION("Calling more specialized ctors")
     {
-        DelegatingConstructors object(true);
+        SECTION("Default Ctor")
+        {
+            SampleB object;
 
-        CHECK(object.GetBoolean() == true);
-        CHECK(object.GetIntA() == 33);
-        CHECK(object.GetIntB() == 44);
-    }
+            CHECK(object.GetBoolean() == true);
+            CHECK(object.GetIntA() == 11);
+            CHECK(object.GetIntB() == 22);
+        }
 
-    SECTION("Ctor 2")
-    {
-        DelegatingConstructors object(true, 55);
+        SECTION("Ctor 1")
+        {
+            SampleB object(true);
 
-        CHECK(object.GetBoolean() == true);
-        CHECK(object.GetIntA() == 55);
-        CHECK(object.GetIntB() == 66);
-    }
+            CHECK(object.GetBoolean() == true);
+            CHECK(object.GetIntA() == 33);
+            CHECK(object.GetIntB() == 44);
+        }
 
-    SECTION("Ctor 3")
-    {
-        DelegatingConstructors object(true, 77, 88);
+        SECTION("Ctor 2")
+        {
+            SampleB object(true, 55);
 
-        CHECK(object.GetBoolean() == true);
-        CHECK(object.GetIntA() == 77);
-        CHECK(object.GetIntB() == 88);
+            CHECK(object.GetBoolean() == true);
+            CHECK(object.GetIntA() == 55);
+            CHECK(object.GetIntB() == 66);
+        }
+
+        SECTION("Ctor 3")
+        {
+            SampleB object(true, 77, 88);
+
+            CHECK(object.GetBoolean() == true);
+            CHECK(object.GetIntA() == 77);
+            CHECK(object.GetIntB() == 88);
+        }
     }
 }
 
