bc957a865ddd4960929e3767b8a24fbe58b4351d
Eric Parayre <eric.parayre@gmail.com>
2017-01-04 23:50:48 -0500

    test nested namespaces

---
+++ source/features/NestedNamespaces.cpp
@@ -0,0 +1,18 @@
+#include "Playground.hpp"
+
+namespace A::B::C
+{
+    //-----------------------------------------------------------------------------
+    int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+//-----------------------------------------------------------------------------
+TEST_CASE( "Nested Namespaces", "[c++17][nested_namespaces]" )
+{
+    CHECK( A::B::C::Add(1, 2) == 3 );
+    CHECK( A::B::C::Add(2, 3) == 5 );
+    CHECK( A::B::C::Add(3, 4) == 7 );
+}
