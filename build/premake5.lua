6e673d15f3ca3f342efe9261406450edbee0dfab
Eric Parayre <eric.parayre@gmail.com>
2017-07-18 22:28:18 -0400

    appveyor * test results

--- build/premake5.lua
+++ build/premake5.lua
@@ -1,6 +1,14 @@
 
 local Playground = {}
 
+function Playground.CatchArguments()
+    if os.getenv("APPVEYOR") then
+        return " --reporter junit --out $(OutDir)$(TargetName).results.xml"
+    else
+        return ""
+    end
+end
+
 function Playground.GenerateSolution()
     Playground.RootDir   = path.getabsolute( ".." );
     Playground.SourceDir = path.join( Playground.RootDir, "source" )
@@ -67,6 +75,7 @@ function Playground.GenerateProject()
         configuration { "vs*" }
             buildoptions { "/wd4706" }
             postbuildcommands { "\"$(TargetPath)\"" }
+            postbuildcommands { "\"$(TargetPath)\"" .. Playground.CatchArguments() }
 end
 
 if _ACTION then
